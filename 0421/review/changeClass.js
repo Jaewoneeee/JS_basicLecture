30988a222f5c974efa9107ed193f93fc6f1ad801
jaewoneeee <chou60400@naver.com>
2022-04-22 00:13:40 +0900

    feat : code repactoring

--- 0421/review/changeClass.js
+++ 0421/review/changeClass.js
@@ -1,13 +1,12 @@
-
-const canvas = document.getElementById('myCanvas');      // 이 아이디를 가지고 특정 탭에 접근을 한다
-const context = canvas.getContext('2d');            // context라는 존재를 통해 그리기를 한다
+const canvas = document.getElementById("myCanvas"); // 이 아이디를 가지고 특정 탭에 접근을 한다
+const context = canvas.getContext("2d"); // context라는 존재를 통해 그리기를 한다
 
 // keydown
-document.addEventListener('keydown', keyDownEeventHandler);
-document.addEventListener('keyup', keyDownEeventHandler); // 자연스러움을 추가하기 위해 쓸 수 있다. 
+document.addEventListener("keydown", keyDownEeventHandler);
+document.addEventListener("keyup", keyDownEeventHandler); // 자연스러움을 추가하기 위해 쓸 수 있다.
 
 // // arc
-// let arcPosX = canvas.width / 2 
+// let arcPosX = canvas.width / 2
 // let arcPosY = canvas.height / 2 + barHeight
 // // let arcPosX = canvas.width / 2
 // // let arcPosY = canvas.height / 2
@@ -15,17 +14,16 @@ document.addEventListener('keyup', keyDownEeventHandler); // 자연스러움을 
 
 // bar
 let barWidth = 250;
-let barHeight = 40
-let barPosX = canvas.width / 2 - barWidth / 2
-let barPosY = canvas.height - barHeight / 2
+let barHeight = 40;
+let barPosX = canvas.width / 2 - barWidth / 2;
+let barPosY = canvas.height - barHeight / 2;
 
 // arc
 const arcRaius = 20;
-let arcPosX = canvas.width / 2 
-let arcPosY = canvas.height - barHeight / 2 - arcRaius
+let arcPosX = canvas.width / 2;
+let arcPosY = canvas.height - barHeight / 2 - arcRaius;
 // let arcPosX = canvas.width / 2
 // let arcPosY = canvas.height / 2
-
 
 // 방향에 대한 변수 설정
 // ball
@@ -39,86 +37,83 @@ let barSpeed = 40;
 
 //객체생성
 let ball = {
-    left : 0,
-    right : 0, 
-    top : 0, 
-    bottom : 0
-}
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+};
 
 let paddle = {
-    left : 0,
-    right : 0, 
-    top : 0, 
-    bottom : 0
-}
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+};
 
 // 벽돌만들기
 // let brick = {  //이렇게 구조를 잡겠다는 형태
 //     left : 0,
-//     right : 0, 
-//     top : 0, 
+//     right : 0,
+//     top : 0,
 //     bottom : 0,
 //     column : 0,
-//     row : 0    
+//     row : 0
 // }
 
-// 블록만들기 
+// 블록만들기
 const brickWidth = 50; // 간격 10
-const brickHeight = 25 // 간격 5
+const brickHeight = 25; // 간격 5
 const brickColumn = 5;
 const brickRow = 4;
 let clearCount = 0;
 // let brickArray = []
-let brickArray  // class로 바꾸며 그냥 선언만 해주자
+let brickArray; // class로 바꾸며 그냥 선언만 해주자
 
-function setBricks() 
-{
-    brickArray = [] // why? 여기서 다시 선언해주는거지?  
+function setBricks() {
+  brickArray = []; // why? 여기서 다시 선언해주는거지?
 
-    for(let i = 0; i < brickRow; i++){
-        brickArray[i] = []
-        for(let j = 0; j < brickColumn; j++){
+  for (let i = 0; i < brickRow; i++) {
+    brickArray[i] = [];
+    for (let j = 0; j < brickColumn; j++) {
+      // 기존코드
+      // brickArray[i][j] = {
+      //     // 여기 숫자 의미를 잘 파악해야한다
+      //     left : 55 + j * (brickWidth + 10),
+      //     right : 55 + j * (brickWidth + 10) + 50, //60으로 나중에 묶기
+      //     top : 30 + i * (brickHeight + 5),
+      //     bottom : 30 + i * (brickHeight + 5) + 25,
+      //     row : j,
+      //     column : i,
+      //     isAlive : true, // 여기에 변수를 추가하는 개념
 
-            // 기존코드 
-            // brickArray[i][j] = {
-            //     // 여기 숫자 의미를 잘 파악해야한다 
-            //     left : 55 + j * (brickWidth + 10),
-            //     right : 55 + j * (brickWidth + 10) + 50, //60으로 나중에 묶기
-            //     top : 30 + i * (brickHeight + 5), 
-            //     bottom : 30 + i * (brickHeight + 5) + 25,
-            //     row : j,
-            //     column : i,
-            //     isAlive : true, // 여기에 변수를 추가하는 개념 
-
-            // class로 수정해보자
-            // 미리 선언해둔 각각의 class인자에 값을 넣어주는것.
-            brickArray[i][j] = new Brick(
-                55 + j * (brickWidth + 10), // this.left
-                30 + i * (brickHeight + 5), // this.top
-                55 + j * (brickWidth + 10) + 50, // this.right
-                30 + i * (brickHeight + 5) + 25, // this.bottom
-                'green' // this.color
-            )
-        }
+      // class로 수정해보자
+      // 미리 선언해둔 각각의 class인자에 값을 넣어주는것.
+      brickArray[i][j] = new Brick(
+        55 + j * (brickWidth + 10), // this.left
+        30 + i * (brickHeight + 5), // this.top
+        55 + j * (brickWidth + 10) + 50, // this.right
+        30 + i * (brickHeight + 5) + 25, // this.bottom
+        "green" // this.color
+      );
     }
-    //console.log(brickArray[1][3].left) // 이게 결국 함수 안에서만 호출됐기 때문에.. 어떻게 할 수가 없네; 
+  }
 }
 
 // ball과 bar가 만나지 않는 경우 설정
-function isCollisionRectToRect(rectA, rectB)
-{
-    // a의 왼쪽과 b의 오른쪽
-    // a의 오른과 b의 왼쪽
-    // a의 아래쪽과 b의 위쪽
-    // a의 위쪽과 b의 아래쪽
-    if(rectA.left > rectB.right ||
-       rectA.right < rectB.left ||
-       rectA.top > rectB.bottom ||
-       rectA.bottom < rectB.top)
-       {
-           return false 
-       }
-    return true;
+function isCollisionRectToRect(rectA, rectB) {
+  // a의 왼쪽과 b의 오른쪽
+  // a의 오른과 b의 왼쪽
+  // a의 아래쪽과 b의 위쪽
+  // a의 위쪽과 b의 아래쪽
+  if (
+    rectA.left > rectB.right ||
+    rectA.right < rectB.left ||
+    rectA.top > rectB.bottom ||
+    rectA.bottom < rectB.top
+  ) {
+    return false;
+  }
+  return true;
 }
 
 //let clearCount = 0;
@@ -126,178 +121,160 @@ let startClick = true;
 
 // bar 움직이기
 function keyDownEeventHandler(e) {
-    if(e.key == 'ArrowRight' && barPosX + barWidth < canvas.width ) {
-        // 바를 오른쪽으로 이동
-        //console.log(alert("오른쪽 된다!"))
-         barPosX += barMoveDirX;
-         //arcPosX += barMoveDirX
+  if (e.key == "ArrowRight" && barPosX + barWidth < canvas.width) {
+    // 바를 오른쪽으로 이동
+    //console.log(alert("오른쪽 된다!"))
+    barPosX += barMoveDirX;
+    //arcPosX += barMoveDirX
+  } else if (e.key == "ArrowLeft" && barPosX > 0) {
+    //console.log(alert("왼쪽 된다!"))
+    barPosX -= barMoveDirX;
+    //arcPosX -= barMoveDirX
+  }
 
-    } else if (e.key == 'ArrowLeft' && barPosX > 0) {
-        //console.log(alert("왼쪽 된다!"))
-        barPosX -= barMoveDirX;
-        //arcPosX -= barMoveDirX     
-    }
-    
-    if(e.key == 'ArrowRight' && startClick  && barPosX + barWidth < canvas.width ) {
+  if (
+    e.key == "ArrowRight" &&
+    startClick &&
+    barPosX + barWidth < canvas.width
+  ) {
+    arcPosX += barMoveDirX;
+  } else if (e.key == "ArrowLeft" && startClick && barPosX > 0) {
+    arcPosX -= barMoveDirX;
+  }
 
-        arcPosX += barMoveDirX
+  if (e.key == " " && startClick) {
+    setInterval(update, 10);
+    startClick = false;
+  }
 
-    } else if (e.key == 'ArrowLeft' && startClick && barPosX > 0)  {
-        
-        arcPosX -= barMoveDirX     
-
-    }
-
-    if(e.key == ' ' && startClick ){
-        setInterval(update, 10);   
-        startClick = false;
-    }
-
-
-    paddle.left = barPosX;
-    paddle.right = barPosX + barWidth;
-    paddle.top = barPosY;
-    paddle.bottom = barPosY + barHeight;
+  paddle.left = barPosX;
+  paddle.right = barPosX + barWidth;
+  paddle.top = barPosY;
+  paddle.bottom = barPosY + barHeight;
 }
 
 // ball 움직이기
 function update() {
-    if(arcPosX - arcRaius < 0 ) {
-        arcMoveDirX = 1;
-        
-    } else if(arcPosX + arcRaius > canvas.width){
-        arcMoveDirX = -1;
-    }
+  if (arcPosX - arcRaius < 0) {
+    arcMoveDirX = 1;
+  } else if (arcPosX + arcRaius > canvas.width) {
+    arcMoveDirX = -1;
+  }
 
-    if(arcPosY - arcRaius < 0) {
+  if (arcPosY - arcRaius < 0) {
+    arcMoveDirY = -1;
+  } else if (arcPosY + arcRaius > canvas.height) {
+    location.reload();
+    alert("Game Over!!");
+  }
+
+  //arcSpeed+=0.01;
+  arcPosX += arcMoveDirX * arcSpeed;
+  arcPosY -= arcMoveDirY * arcSpeed;
+
+  ball.left = arcPosX - arcRaius;
+  ball.right = arcPosX + arcRaius;
+  ball.top = arcPosY - arcRaius;
+  ball.bottom = arcPosY + arcRaius;
+
+  // 충돌확인
+  if (isCollisionRectToRect(ball, paddle)) {
+    arcMoveDirY = 1;
+    //arcMoveDirX = -1;
+    //arcPosY = paddle.top - arcRaius;
+  }
+
+  for (let i = 0; i < brickRow; i++) {
+    for (let j = 0; j < brickColumn; j++) {
+      if (
+        brickArray[i][j].isAlive &&
+        isCollisionRectToRect(ball, brickArray[i][j])
+      ) {
+        // 벽돌을 안보이게.. 위치를 바꾸던지.. ball의 방향을 바꾸던지
+        // console.log(i,j)
+        brickArray[i][j].isAlive = false;
         arcMoveDirY = -1;
-    } else if(arcPosY + arcRaius > canvas.height) {
-        location.reload();
-        alert("Game Over!!");
+        clearCount++;
+        console.log(clearCount);
+        break; // 벽돌 중복으로 뿌서지는거 방지
+      }
     }
+  }
 
-    //arcSpeed+=0.01;
-    arcPosX += arcMoveDirX * arcSpeed;
-    arcPosY -= arcMoveDirY * arcSpeed;
-
-    ball.left = arcPosX - arcRaius ;
-    ball.right = arcPosX + arcRaius 
-    ball.top = arcPosY - arcRaius 
-    ball.bottom = arcPosY + arcRaius
-    
-
-    // 충돌확인
-    if(isCollisionRectToRect(ball, paddle)){
-        arcMoveDirY = 1;
-        //arcMoveDirX = -1;
-        //arcPosY = paddle.top - arcRaius;
-    }
-
-   
-
-    for(let i = 0;  i < brickRow; i++)
-    {
-        for(let j = 0; j < brickColumn; j++)
-        {
-            if(brickArray[i][j].isAlive && isCollisionRectToRect(ball, brickArray[i][j]))
-            {
-                // 벽돌을 안보이게.. 위치를 바꾸던지.. ball의 방향을 바꾸던지 
-                // console.log(i,j)
-                brickArray[i][j].isAlive = false;   
-                arcMoveDirY = -1
-                clearCount++
-                console.log(clearCount)
-                break // 벽돌 중복으로 뿌서지는거 방지                
-
-            }
-        }
-    }
-
-    if(clearCount==20)
-    {
-        setTimeout(function() {
-            alert('Clear!',location.reload());
-          }, 100);
-    }
+  if (clearCount == 20) {
+    setTimeout(function () {
+      alert("Clear!", location.reload());
+    }, 100);
+  }
 }
 
 // clear하는 새로운 함수 (교수님 버젼)
 function checkToWin() {
-    
-    // 1. bricks배열에 있는 정보로 처리
-    // setBricks[0][0].isAlive
+  // 1. bricks배열에 있는 정보로 처리
+  // setBricks[0][0].isAlive
 
-    let flatBricks = brickArray.flat();
-    // flat을 사용하면 2차원 안에 있는 인덱스 값을 하나하나 다 뺴서 하나의 배열로 만들어준다.(1차원배열로)
+  let flatBricks = brickArray.flat();
+  // flat을 사용하면 2차원 안에 있는 인덱스 값을 하나하나 다 뺴서 하나의 배열로 만들어준다.(1차원배열로)
 
-    let deadBricks = flatBricks.filter(brick => brick.isAlive == false);
-    if (deadBricks.length ==  brickRow * brickColumn)
-    {
-        // 게임 클리어
-        alert('게임 클리어!')
-    }
+  let deadBricks = flatBricks.filter((brick) => brick.isAlive == false);
+  if (deadBricks.length == brickRow * brickColumn) {
+    // 게임 클리어
+    alert("게임 클리어!");
+  }
 
-    // 2. 카운트를 세는 변수를 만들어서 처리 
-    if(daadBricksCount == brickRow * brickColumn){
-
-    }
+  // 2. 카운트를 세는 변수를 만들어서 처리
+  if (daadBricksCount == brickRow * brickColumn) {
+  }
 }
-
 
 // ==================================== 화면 그리기(도형) ======================================
 function draw() {
- 
-    // 화면 클리어
-    context.clearRect(0, 0, canvas.width, canvas.height);
+  // 화면 클리어
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
-    // 다른 도형 그리기
-    drawBar();
-    drawArc();
-    drawBricks() //블록 그리기 추가
+  // 다른 도형 그리기
+  drawBar();
+  drawArc();
+  drawBricks(); //블록 그리기 추가
 }
 
 // ball 그리기
 function drawArc() {
-
-    context.beginPath(); // 그리기를 시작하겠다
-                // x    ,   y      , radius  , startAngle, endAngle
-    context.arc(arcPosX , arcPosY  , arcRaius, 0, 2 * Math.PI);
-    context.fillStyle = 'blue'; // 색깔 고르고
-    context.fill(); // 채우기
-    context.closePath(); // 그리기를 끝내겠다     
+  context.beginPath(); // 그리기를 시작하겠다
+  // x    ,   y      , radius  , startAngle, endAngle
+  context.arc(arcPosX, arcPosY, arcRaius, 0, 2 * Math.PI);
+  context.fillStyle = "blue"; // 색깔 고르고
+  context.fill(); // 채우기
+  context.closePath(); // 그리기를 끝내겠다
 }
 
 // bar 그리기
-function drawBar(){
-
-    context.beginPath(); // 그리기를 시작하겠다
-                //  x    ,    y   ,   width ,  height
-    context.rect( barPosX, barPosY, barWidth, barHeight);
-    context.fillStyle = 'red'; // 색깔 고르고
-    context.fill(); // 채우기
-    context.closePath(); // 그리기를 끝내겠다
+function drawBar() {
+  context.beginPath(); // 그리기를 시작하겠다
+  //  x    ,    y   ,   width ,  height
+  context.rect(barPosX, barPosY, barWidth, barHeight);
+  context.fillStyle = "red"; // 색깔 고르고
+  context.fill(); // 채우기
+  context.closePath(); // 그리기를 끝내겠다
 }
 
 // block 그리기
-function drawBricks() 
-{
-    context.beginPath();
-    for(let i = 0; i < brickRow; i++)
-    {
-        for(let j = 0; j < brickColumn; j++)
-        {
-            // if(brickArray[i][j].isAlive)
-            // {
-            //     context.rect(brickArray[i][j].left, brickArray[i][j].top, brickWidth, brickHeight)
-            //     context.fillStyle = 'coral'; // 이때 색칠하는것을 for문 안에 넣느냐 마느냐로 블록색을 여러개로 줄 수 있다.
-            //     context.fill();
-            // }
+function drawBricks() {
+  context.beginPath();
+  for (let i = 0; i < brickRow; i++) {
+    for (let j = 0; j < brickColumn; j++) {
+      // if(brickArray[i][j].isAlive)
+      // {
+      //     context.rect(brickArray[i][j].left, brickArray[i][j].top, brickWidth, brickHeight)
+      //     context.fillStyle = 'coral'; // 이때 색칠하는것을 for문 안에 넣느냐 마느냐로 블록색을 여러개로 줄 수 있다.
+      //     context.fill();
+      // }
 
-            // 위 조건문을 주석처리하고, 밑에서 선언해준 class에있는 함수만 불러내주면 된다.
-            brickArray[i][j].draw();
-        }
+      // 위 조건문을 주석처리하고, 밑에서 선언해준 class에있는 함수만 불러내주면 된다.
+      brickArray[i][j].draw();
     }
-    context.closePath();
+  }
+  context.closePath();
 }
 
 // class로 새롭게 접근해보자
@@ -306,36 +283,31 @@ function drawBricks()
 // 바꾼 순서나 흐름을 잘 생각해봐야함
 
 class Brick {
-    // # 속성에 해당하는 내용
-    // 처음 위에서 배열을 만들때 설정해줬던 값을 이쪽으로 빼준거라고 할수 있을듯 
-    // 여기서 먼저 class로 설정을 해두고, 이제 위에서는 this.로 가져다 쓰기만 하면되지
-    constructor(left, top, right, bottom, color){
-        this.left = left;
-        this.top = top;
-        this.right = right;
-        this.bottom = bottom;
-        this.left = left;
-        this.isAlive = true;
-        this.color = color;
+  // # 속성에 해당하는 내용
+  // 처음 위에서 배열을 만들때 설정해줬던 값을 이쪽으로 빼준거라고 할수 있을듯
+  // 여기서 먼저 class로 설정을 해두고, 이제 위에서는 this.로 가져다 쓰기만 하면되지
+  constructor(left, top, right, bottom, color) {
+    this.left = left;
+    this.top = top;
+    this.right = right;
+    this.bottom = bottom;
+    this.left = left;
+    this.isAlive = true;
+    this.color = color;
+  }
+
+  // # 기능에 해당하는 내용
+  // 위에서 draw로 그렸던 내용을 이쪽 함수에 선언하고 이 함수를 이제 가져다 쓰도록 하자
+  // 일단 rect안에 들어가는 인자값이 위에서 바꼈기 때문에 배열먼저 만들고 다시 내려오자
+  draw() {
+    // 위 class에서 선언해준 것들로 변수를 바꿔주자
+    if (this.isAlive) {
+      context.rect(this.left, this.top, brickWidth, brickHeight);
+      context.fillStyle = this.color;
+      context.fill();
     }
-    
-    // # 기능에 해당하는 내용
-    // 위에서 draw로 그렸던 내용을 이쪽 함수에 선언하고 이 함수를 이제 가져다 쓰도록 하자
-    // 일단 rect안에 들어가는 인자값이 위에서 바꼈기 때문에 배열먼저 만들고 다시 내려오자
-    draw() {
-        // 위 class에서 선언해준 것들로 변수를 바꿔주자
-        if(this.isAlive) 
-        {
-            context.rect(this.left, this.top, brickWidth, brickHeight)
-            context.fillStyle = this.color; 
-            context.fill();
-        }
-    }
+  }
 }
 
-
-
 setInterval(draw, 10);
-// 동적 움직임을 위해 코드 추가
-//setInterval(update, 10);
 setBricks();
--- 0421/review/class.js
+++ 0421/review/class.js
@@ -15,8 +15,7 @@
 
 // // ===================
 // // 위에 코드에서 함수를 따로 빼줘서 prototype으로 설정을 해주면?
-// // 프로토타입이 그래서 뭐야? 암튼 안쓰게됨
-// // 메모리 관점에서 자꾸 쌓이게 되니까 좋지 않음?
+// // 메모리 관점에서 자꾸 쌓이게 되니까 좋지 않음
 
 // Brick.prototype.movingAction = function () { this.left++; console.log('움직이는중!')}
 
@@ -27,39 +26,38 @@
 
 // ====================
 // ES6 이후 class가 도입됨에 따라 prototype을 굳이 쓸 필요가 없어짐.
-// class 훨씬 더 직관적이라는 장점이 있구만 
+// class 훨씬 더 직관적이라는 장점이 있구만
 class Brick2 {
-    // 속성에 해당하는 내용
-    constructor(left, top, right, bottom){
-        this.left = left;
-        this.top = top;
-        this.right = right;
-        this.bottom = bottom;
-    }
+  // 속성에 해당하는 내용
+  constructor(left, top, right, bottom) {
+    this.left = left;
+    this.top = top;
+    this.right = right;
+    this.bottom = bottom;
+  }
 
-    // 기능에 해당하는 내용
-    // movingAction() {
-    //     this.left++; console.log('움직이는중!')
-    // }
+  // 기능에 해당하는 내용
+  // movingAction() {
+  //     this.left++; console.log('움직이는중!')
+  // }
 }
 
 // 위처럼 한번에 할 수도 있고 아래와 같이 상속 개념으로 빼줄수 도 있음
 
-// 요론걸 이제 상속이라고 함 
+// 이런걸 이제 상속이라고 함
 class MovingBrick extends Brick2 {
-    // 기능에 해당하는 내용
-    movingAction2() {
-        this.left++; 
-        this.top++;
-        console.log('움직이는중!');
-    }
+  // 기능에 해당하는 내용
+  movingAction2() {
+    this.left++;
+    this.top++;
+    console.log("움직이는중!");
+  }
 }
 
 // MovingBrick();
-let test = new Brick2(130, 20, 410, 10)
-let test2 = new MovingBrick(3, 0, 0, 0)
-console.log(test.left)
-console.log(test)
+let test = new Brick2(130, 20, 410, 10);
+let test2 = new MovingBrick(3, 0, 0, 0);
+console.log(test.left);
+console.log(test);
 test2.movingAction2();
-console.log(test2)
-
+console.log(test2);
